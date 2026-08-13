397eabf04183c16577e29049179d565deef22d4c
Ignacio <i.bravosalgado@uandresbello.edu>
2026-08-13 12:51:58 -0400

    primero

---
+++ hello.py
@@ -0,0 +1,33 @@
+print("Hola mundo nuevo")
+print("Hola mundo nuevo 2\n Mi nombre es python") # \n  sirve para hacer un salto de linea
+
+
+numero = 10
+print(numero)
+print(type(numero))                               # type sirve para saber el tipo de dato que es la variable
+
+
+cadena = "ESCRIBIR ALGO"
+print(cadena)
+
+
+num1 = 10
+num2 = 20
+resultado = (num1 + num2) * 10
+print("El resultado es: ",resultado)
+
+
+
+
+
+
+
+
+''' operadores racionales
+< mayor que
+> menor que 
+>= mayor o igual que
+<= menor o igual que
+== igual que
+!= diferente que
+'''
