39b3dad702cbc4628ea180e53485b0cf0b6086e1
Ignacio <i.bravosalgado@uandresbello.edu>
2026-08-20 12:11:55 -0400

    iemdi

--- ATS/eje2.py
+++ ATS/eje2.py
@@ -1,3 +1,4 @@
+'''
 numero = int(input("ingrese su edad:"))
 if(numero >= 18)and(numero<=99):
     print("Es mayor de edad")
@@ -21,5 +22,62 @@ else:
 
 #eje 2
 # hacer un programa que pida 3 numeros y determine cual es el mayor
+num3 = int(input("Ingrese el primer nuemro:"))
+num4 = int(input("Ingrese el segundo numero:"))
+num5 = int(input("Ingrese el tercer numero:"))
+
+if(num3>num4) and (num3>num5):
+    print(f"El numero 1: {num3} es el mayor de todos ")
+elif(num4>num3) and (num4>num5):
+    print(f"El numero 2: {num4} es el mayor de todos")
+elif(num5>num3) and (num5>num4):
+    print(f"El numero 3: {num5} es el mayor a todos")
+else:
+    print("Todos los numeros son igualees")
+
+#eje3 
+# Hacer un programa que pida un caracter e indique si es una vocal o no 
+letra = input("Ingrese un caracter:")
+if(letra=="a") or (letra=="e") or (letra=="i") or (letra=="o") or (letra=="u"):
+    print("La letra pertenece una vocal")
+elif(letra=="A") or (letra=="E") or (letra=="I") or (letra=="O") or (letra=="U"):
+    print("La letra pertenece a una vocal mayuscula")
+else:
+    print("LA letra No pertenece a ninguna vocal")
+
+# Si ingresamos en letra = input("Ingrese un caracter:").lower()
+# .lower() --> transformar todo a minuscula
+# .upper() --> transformar todo a mayuscula
 
 
+#eje 4
+# Contruir un programa que simule el funcionamiento de una calculadora que puede realizar las 4 operaciones
+# el ususuario debe especificar la operacion con el primer caracter del nombre de la operacion
+print("Bienvenido a la calculadora")
+print("Ingrese S,s --> Suma")
+print("Ingrese R,r --> Resta")
+print("Ingrese M,n --> Multiplicacion")
+print("Ingrese D,d --> Division")
+caracter = input("Ingrese la operacion que desea:")
+num1 = float(input("Ingrese el primer numero:"))
+num2 = float(input("Ingrese el segundo numero:"))
+if(caracter=="S") or (caracter=="s"):
+    resul = num1 + num2
+    print(f"El resultado de la Suma de ambos numeros es: {resul}")
+elif(caracter=="R") or (caracter=="r"):
+    resul = num1 - num2
+    print(f"El resultado de la Suma de ambos numeros es: {resul}")
+elif(caracter=="M") or (caracter=="m"):
+    resul = num1 * num2
+    print(f"El resultado de la Suma de ambos numeros es: {resul}")
+elif(caracter=="D") or (caracter=="d"):
+    resul = num1 / num2
+    print(f"El resultado de la Suma de ambos numeros es: {resul}")
+else:
+    print("No ingreso ningun caracter recomendado")
+    
+'''
+#eje 5
+# Hacer un programa que simule un cajero con uun saldo inicial de $1000 y tendra
+# 1. Ingresar dinero a la cuenta. 2. Retirar dinero de la cuenta 
+# 3. Mostrar dinero disponible. 4. Salir
